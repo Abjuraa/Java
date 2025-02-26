804575c42f6c4debef64167ffd0804c53b38d55b
Abjura <107774277+Abjuraa@users.noreply.github.com>
2025-02-26 18:36:10 -0500

    Creando menu para los pacientes

--- src/ui/UIMenu.java
+++ src/ui/UIMenu.java
@@ -30,4 +30,31 @@ public class UIMenu {
             }
         } while (response != 0);
     }
+
+    static void showPatientMenu(){
+        int response = 0;
+        do {
+            System.out.println("Paciente");
+            System.out.println("Ingrese una opción");
+            System.out.println("1. Apuntes de libros");
+            System.out.println("2. Mis apuntes");
+            System.out.println("0. Atras");
+
+            Scanner sc = new Scanner(System.in);
+            response = Integer.parseInt(sc.nextLine());
+
+            switch(response) {
+                case 1:
+                    System.out.println("::Book an appointment");
+                    break;
+                case 2:
+                    System.out.println("::My appointments");
+                    break;
+                case 0:
+                    showMenu();
+                    break;
+            }
+        } while(response != 0);
+
+    }
 }
