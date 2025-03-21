507f142031f78062efc41c3c1c8728526e8969fb
Abjura <107774277+Abjuraa@users.noreply.github.com>
2025-03-20 21:00:05 -0500

    Clases anidadas

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,11 +1,11 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
     //Atributos
-    static int id = 0;
-    String name;
-    String speciality;
-
-    Doctor() {
-    }
+    private static int id = 0;
+    private String name;
+    private String speciality;
 
     Doctor(String name, String speciality) {
         setName(name);
@@ -13,8 +13,6 @@ public class Doctor {
         id++;
         this.showName();
         this.showSpeciality();
-        this.showId();
-        System.out.println("-------------------------");
     }
 
     //Getters
@@ -60,5 +58,46 @@ public class Doctor {
         return new StringBuilder().append("El doctor es ").append(this.name).append(" y se especializa en ").append(this.speciality).toString();
     }
 
+    //Lista que guarda objetos de tipo AvailableAppointment
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+
+    //Metodo que agrega una nueva cita a la lista de citas
+    public void addAvailableAppointment(Date date, String time) {
+        availableAppointments.add(new AvailableAppointment(date, time));
+    }
+
+    //Devuelve la lista completa de citas disponibles
+    public ArrayList<AvailableAppointment> getAvailableAppointments() {
+        return availableAppointments;
+    }
+
+    public static class AvailableAppointment {
+        private int id;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time) {
+            setDate(date);
+            setTime(time);
+        }
+
+        //Getters
+        public Date getDate() {
+            return this.date;
+        }
+
+        public String getTime() {
+            return this.time;
+        }
+
+        //Setters
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
         Doctor myDoctor = new Doctor("Bryam", "Optometria");
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10pm");
+        myDoctor.addAvailableAppointment(new Date(), "5am");
+
+        //Trae todas las citas en la clase Doctor y solo trae las citas que le pertenecen a myDoctor
+        for (Doctor.AvailableAppointment citas : myDoctor.getAvailableAppointments()) {
+            System.out.println(citas.getDate() + " " + citas.getTime());
+        }
         System.out.println(myDoctor);
 
         Patient myPatient = new Patient("leonel", "messi@gmail.com");
@@ -10,6 +20,6 @@ public class Main {
         myPatient.setWeight(51.3);
         System.out.println(myPatient.getWeight());
         System.out.println(myPatient.getPhoneNumber());
-        //showMenu();
+        //showMenu();*/
     }
 }
